13e31dcf59d372595cdc138f25b5680f1cb726cf
David Li <david.li@activenetwork.com>
2018-01-11 10:11:09 +0800

    add user identifier example code

--- Example/DynamicStatistics/DSAppDelegate.m
+++ Example/DynamicStatistics/DSAppDelegate.m
@@ -7,12 +7,14 @@
 //
 
 #import "DSAppDelegate.h"
-#import <DynamicStatistics/DynamicStatistics.h>
+#import "DynamicStatistics.h"
+#import "DSUserIdentifier.h"
 
 @implementation DSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"User identifier: %@", [DSUserIdentifier userUUID]);
     [[DynamicStatistics sharedInstance] setupWithPlist:@"DynamicStatistics" andEventLogBlock:^(DSViewEvent *event) {
         NSLog(@"\n--------------------------------------------------------------\nGet View Event\nEvent Name: %@\nEvent Type: %@\nView Path:%@\n--------------------------------------------------------------", event.eventName, event.eventTypeDescription, event.viewPath);
     }];
